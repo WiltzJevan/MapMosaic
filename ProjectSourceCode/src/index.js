3466e903aad25b642db4a03b2ca90356d177db23
root <root@MSI-Wiltz>
2025-04-15 17:32:12 -0600

    Debugging

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -69,6 +69,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/data', express.static(path.join(__dirname, 'resources/data')));
 
 app.use(
   session({
--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -1,1 +1,30 @@
 (WIP)
+fetch('/data/world.geojson')
+  .then(response => response.json())
+  .then(geojson => {
+    const countryCoords = {};
+    geojson.features.forEach(feature => {
+      const name = feature.properties.name;
+      const coordinates = feature.geometry.coordinates;
+      // Calculate centroid for the country
+      const centroid = turf.centroid(feature).geometry.coordinates;
+      countryCoords[name] = centroid;
+    });
+
+    // Now you can use countryCoords to place markers
+    trips.forEach(trip => {
+      const country = trip.country_name || trip.location;
+      const coords = countryCoords[country];
+
+      if (coords && trip.image_path) {
+        const el = document.createElement('div');
+        el.className = 'trip-marker';
+        el.style.backgroundImage = `url('${trip.image_path}')`;
+
+        new mapboxgl.Marker(el)
+          .setLngLat(coords)
+          .setPopup(new mapboxgl.Popup({ offset: 25 }).setText(trip.title || country))
+          .addTo(map);
+      }
+    });
+  });
